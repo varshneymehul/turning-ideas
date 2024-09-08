da1bb7a0003033464d18816b5d5663efd503bee1
Mehul Varshney <varshneymehul5@gmail.com>
2024-09-08 18:24:15 +0530

    enable vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,10 @@ import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
+
+
 const smooch = Smooch({
   subsets: ["latin"],
   weight: "400",
