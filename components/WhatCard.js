d272edbea98ea783afe0b30278c444cc7765cbf5
Mehul Varshney <varshneymehul5@gmail.com>
2024-09-09 22:52:37 +0530

    fix horizontal scroll on why card

--- components/WhatCard.js
+++ components/WhatCard.js
@@ -6,8 +6,7 @@ const WhatCard = ({ type, text }) => {
       <h1 className="text-2xl py-2 sm:text-3xl md:text-4xl font-forum uppercase text-sky-900">
         {type === "what" ? "What?" : "How?"}
       </h1>
-      <p className="absolute text-9xl -z-10 block text-gray-100 uppercase">
-
+      <p className="absolute text-8xl  -z-10 block text-gray-100 uppercase overflow-hidden">
         {type === "what" ? "What?" : "How?"}
       </p>
       <p className="text-lg">{text}</p>
